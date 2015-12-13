a64af7b0b9743d19babf98ae62e6a9423307a590
masta <masta@housemagik.com>
2015-12-13 23:47:45 +0200

    Comment with Notepad

--- help/git.php
+++ help/git.php
@@ -89,3 +89,52 @@ git rm --cached namefile.php
 
 хелп по разделам
 > git help commit
+
+сделать коммит
+> git add .
+> git commit
+переход в другой редактор (VIM)
+- пишем комент сверху всего (например "comment")
+- сохранение
+	-- нажать Esc
+	-- внизу написать ":x" (срхранить и выйти)
+	-- нажать Enter
+- смотрим git log
+............... просмотр ........................
+Vlado@Vlado-PC MINGW64 /d/OpenServer/domains/medin.com ((da922ad...))
+$ git log
+commit da922ad073f3fb753d7ef3b45b16deef2d16eb34
+Author: masta <masta@housemagik.com>
+Date:   Sun Dec 13 22:22:26 2015 +0200
+
+    comment
+
+commit 4c1cf8796b4a5c726264b65f305e815f3b534389
+Author: masta <masta@housemagik.com>
+Date:   Sun Dec 13 00:48:55 2015 +0200
+
+    help
+
+commit 6376d5ad8973e6f7ed3af59c4862471861f8cde4
+Author: masta <masta@housemagik.com>
+Date:   Sat Dec 12 22:42:58 2015 +0200
+
+    init
+................................................
+чтобы вместо VIM использовать редактор Notepad ++
+ввести строку
+> git config --global core.editor "'C:\Program Files (notepad++.exe' -multiInst -notabbar -nosession -noPlugin"
+можно посмотреть, что изменилось в config 
+C:\Users\Vlado\.gitconfig
+................ просмотр ................
+[user]
+	name = masta
+	email = masta@housemagik.com
+
+[core]
+	editor = 'C:\\Program Files (notepad++.exe' -multiInst -notabbar -nosession -noPlugin
+..........................................
+Проверка работы
+> git status (файл непроиндексирован)
+> git add *.php
+> git status (файл проиндексирован)
